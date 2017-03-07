43b3678b180b76a42b6e66200df12efdd2d39cd5
Ramit-Pahwa <ramitpahwa123@gmail.com>
2017-03-07 05:48:31 +0530

    Add Big-M Solver

--- Lab4-BIgM/bigM.cpp
+++ Lab4-BIgM/bigM.cpp
@@ -4,136 +4,189 @@
 #include <string>
 #include <iomanip>
 
-#define M 9999999999
-#define size 15;
-
 using namespace std;
 
-float d[size]={0};
-float mat[size][size], b[size], temp[size][size], constants[size];
-float ans[size][size], z[size];
-int R, C;
-int rank1, rank2, i, j, k, in_var, var, eqn;
-int countVal=0,countVal1=0, slackVar = 0;
-bool unbounded = false;
-bool infinite = false;
-int table_no = 1;
+#define M 10000
+#define size 15
+
+// Variables involved in the Problem 
+
+float mat[size][size],temp[size][size];
+float matconst[size];
+float objectiveCoef[size]={0};
+int rc[size]={0};            //Right coefficient matrix used to indicate artificial variable 
+int slackVar=0,artiVar=0,surpVar=0;
 int max_or_min;
 int solution_index[size];
+bool infinite=false;
+bool unbounded=false;
+int counter=0;
+int n,m,i,j,k;
+int table_no=1;
 
-void parse(){
-    string inequality;  
-    printf("Enter the number of variables\n");
-    scanf("%d",&var);
+void input()
+{
+
+  string inequality;
+  
+  printf("Enter the number of variables\n");
+  scanf("%d",&n);
     
-    printf("Enter number of equations\n");
-    scanf("%d",&eqn);
-
-    for (i=0;i<eqn;i++)
+  printf("Enter number of equations\n");
+  scanf("%d",&m);
+  //needs to be updated 
+  for (i=0;i<m;i++)
     {
-      solution_index[i]=eqn+i+1;
+      solution_index[i]=m+i+1;
     }
 
-    printf("Enter 1 for Maximization problem and -1 for for Minimization problem\n");
-    scanf("%d",&max_or_min);	
+  printf("Enter 1 for Maximization problem and -1 for for Minimization problem\n");
+  scanf("%d",&max_or_min);  
+  for(i=0;i<m;i++)
+  { 
+    cout<<"Enter Sign of the Equation"<<endl;
+    cin>>inequality;
+    cout<<"Enter Coefficients"<<endl;
+    if(inequality.at(0)=='<')
+      { 
 
-    for(i = 0 ; i < eqn ; i++)
-    {
-        printf("Enter coefficients, inequation sign and constant term of equation no %d seperated by spaces:\n" , i + 1);
-        for(j = 0 ; j < var ; j++)
+        for (j=0;j<n;j++)
         {
+          scanf("%f",&mat[i][j]);
+        }
+        slackVar++;
+      }
+    else if (inequality.at(0)=='>')
+    {
+      for (j=0;j<n;j++)
+        {
+          scanf("%f",&mat[i][j]);
+        }
+      for (k=0;k<m;k++)
+        {
+          if(k!=i)
+            {
+              mat[k][j+counter]=0;
+            }
+          else
+              mat[k][j+counter]=-1;
+        }
+      rc[i]=-max_or_min*M;  
+      counter++;
+    }
+    else 
+    {
+      for (j=0;j<n;j++)
+          {
             scanf("%f",&mat[i][j]);
-        }
-        cin >> inequality;
-        if (inequality.at(0) == '<')
-        {
-             slackVar++;
-             scanf("%f", &mat[i][j]);
-        }
-    }
-    cout <<"Enter the coefficients of the " << var;
-    cout <<" variables in the objective function Z in order followed by the constant d" << endl;
-    for(int j = 0; j <= var ; j++){
-          scanf("%f", &mat[eqn][j]);
-          if(j != var)
-               mat[eqn][j] = (-1)*max_or_min*mat[eqn][j];
-    }
-}
 
-int getMinFromLastRow(){
-    int ind = -1;
-    float currMin = 0;
-    for(int i = 0 ; i < var ; i++){
-          if(mat[eqn][i] > 0)
-               continue;  
-          if(mat[eqn][i] == 0){
-               infinite = true;
-               return -1;
-          }           
-          if(mat[eqn][i] < currMin){
-               currMin = mat[eqn][i];
-               ind = i;     
-         }
-    }
-    return ind;            
-}
-
-int getPivotRow(int pivotCol){
-     int ind = -1; 
-     float currMin = size0000;
-     for(int i = 0 ; i < eqn; i++){
-          if(mat[i][pivotCol] <= 0)
-               continue;
-          if((mat[i][var]/mat[i][pivotCol]) < currMin){
-               ind = i;
-               currMin = (mat[i][var]/mat[i][pivotCol]);               
           }
-     }
-     return ind;        
+          rc[i]=-max_or_min*M;
+    }
+
+  }
+  cout<<"Enter the Constant term for the equations"<<endl;  
+  for(i=0;i<m;i++)
+    { 
+      scanf("%f",&mat[i][n+counter]);
+    }
+  cout<<"Enter the Coefficient of the Objective Function"<<endl;
+  
+  for (i=0;i<n;i++)
+  {
+    scanf("%f",&objectiveCoef[i]);
+  }
+  
+
+  for(i=0;i<=n+counter;i++)
+    {
+
+      for(j=0;j<m;j++)
+      {
+        mat[m][i]=mat[m][i]+rc[j]*mat[j][i];
+      }
+      mat[m][i]=mat[m][i]-objectiveCoef[i];
+    }
+
 }
 
+int getMinFromLastRow()
+{
+
+  int index=-1;
+  float minValue=0;
+  for(i=0;i<n+counter;i++)
+  {
+    if(mat[m][i]>0)
+      continue;
+    else if(mat[m][i]==0)
+      {infinite=true;}
+    else if (mat[m][i]<0 )
+    {
+      if (mat[m][i]<minValue)
+      {
+        minValue=mat[m][i];
+        index=i;
+      }
+
+    }
+  }
+  return index;
+}
+int getMinRowIndex(int pivotColumn)
+{
+    int index=-1;
+    float minValue=9999999;
+    for(i=0;i<m;i++)
+    {
+      if(mat[i][pivotColumn]<=0)
+        continue;
+      else if ((mat[i][n+counter]/mat[i][pivotColumn])<minValue)
+        { 
+          minValue=mat[i][n+counter]/mat[i][pivotColumn];
+          index=i;
+          }
+    }
+    return index;
+}
 void makeNewTableau(){
 
   cout << "======================================================" <<endl;
   cout << "\t\t Table " <<::table_no++ << endl << endl;
-  for(int i = 0 ; i <=eqn ; i++){
-    for(int j = 0 ; j <= var; j++){
+  for(int i = 0 ; i <=m ; i++){
+    for(int j = 0 ; j <= n+counter; j++){
         mat[i][j] = temp[i][j];
-        cout << setw(size)<<setprecision(4)<<mat[i][j] << "\t";
+        cout << setw(10)<<mat[i][j] << "\t";
     }     
     cout << endl;
   }
   cout << "======================================================" <<endl;   
 }
 
-void replaceSolution(int pivotCol,int pivotRow)
-{
-  solution_index[pivotRow]=pivotCol+1;
+void solver()
+{ 
 
-}
-
-
-void find_optimum(){  
   cout << "======================================================" <<endl;
   cout << " \t  Initial Simplex Table " << endl << endl;
-     for(int i = 0 ; i <= eqn ; i++){
-          for(int j = 0 ; j <= var; j++){
-               cout << setw(size)<<setprecision(4)<< mat[i][j] << "\t";
+     for(int i = 0 ; i <= m ; i++){
+          for(int j = 0 ; j <= n+counter ; j++){
+               cout << setw(10)<< mat[i][j] << "\t";
            }
            cout << endl; 
      }          
   cout << "======================================================" <<endl;
 
-
-     int pivotRow, pivotCol;
-     while((pivotCol = getMinFromLastRow()) != -1){
-          if((pivotRow = getPivotRow(pivotCol)) == -1){
-               unbounded = true;
-               return;
-          }
-          replaceSolution(pivotCol,pivotRow);
-          for(int i = 0 ; i <= eqn ; i++){
-               for(int j = 0 ; j <= var; j++){
+  int pivotCol,pivotRow;
+  while((pivotCol=getMinFromLastRow())!=-1)
+  {
+    if((pivotRow=getMinRowIndex(pivotCol))==-1)
+    {
+      unbounded=true;
+      return;
+    }
+    // replaceSolution(pivotalColumn,pivotalRow);
+    for(int i = 0 ; i <= m ; i++){
+               for(int j = 0 ; j <= n+counter; j++){
                     if(i == pivotRow && j == pivotCol)
                          temp[i][j] = 1/mat[i][j];
                     else if(i == pivotRow)     
@@ -146,30 +199,27 @@ void find_optimum(){
                }
           }
           makeNewTableau();
-     }
+
+  }
+
 }
 
-int main(){
-    parse();   
-    find_optimum();
+int main()
+{
 
-    cout <<"======================================================" <<endl;
-    if(infinite)
-        printf("There are infinitely many solutions\n");          
-    else if(unbounded)
-        printf("The problem is unbounded\n");
-    else if (max_or_min==1)         
-        {
-          printf("The Maximum value of Z is %f \n", mat[eqn][var]);
-          for (i=0;i<eqn;i++)
-          {
-            std::cout<<"Valur of x"<<solution_index[i]<<" is:  "<<mat[i][var]<<endl;
-          }
-        }
-    else if(max_or_min==-1)
-         printf("The Minimum value of Z is %f \n", -mat[eqn][var]);
+  input();
+  solver();
+  for(i=0;i<=m;i++)
+  {
+    for(j=0;j<=n+counter;j++)
+    {
+      cout<<"   "<<mat[i][j];
+    }
+    cout<<endl;
+  }
+  
 
 
-    cout <<"======================================================" <<endl; 
-    return 0;
+
+
 }
